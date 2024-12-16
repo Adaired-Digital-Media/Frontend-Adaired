415276ebe466b542f8a2467d70cec9614bdd9fc5
Admin . <admin123@adaired61.com>
2024-12-16 15:21:26 +0530

    Robots.txt Updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,14 +4,25 @@ const robots = async () => {
   const siteUrl: string | undefined = process.env.NEXT_PUBLIC_DOMAIN_NAME;
 
   const metaRobots: MetadataRoute.Robots = {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/_next/",    // Disallow Next.js internal resources
-        "/dashboard"   // Disallow the dashboard path
-      ],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/_next/",    // Disallow Next.js internal resources
+          "/dashboard",  // Disallow the dashboard path
+          "/tag/*",
+          "/author/*",
+          "/category/*",
+          "/?*",
+          "/page/*",
+        ],
+      },
+      {
+        userAgent: "ChatGPT-user",
+        disallow: "/",  // Disallow everything for the "ChatGPT-user" user-agent
+      }
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 
