7b05454641d342a928bc97dee17de4055e228253
Kumar <bittu.kumar@adaired61.com>
2024-12-24 15:39:05 +0530

    Logo Changed

--- src/components/Snowfall/index.tsx
+++ src/components/Snowfall/index.tsx
@@ -3,7 +3,17 @@ import React from "react";
 import Snowfall from "react-snowfall";
 
 const SnowfallComponent = () => {
-  return  <Snowfall />;
+  return (
+    <Snowfall
+      changeFrequency={300}
+      snowflakeCount={300}
+      rotationSpeed={[-1.0, 3.0]}
+      color="#dee4fd"
+      speed={[1, 4]}
+      radius={[1, 6]}
+      wind={[-1, 5]}
+    />
+  );
 };
 
 export default SnowfallComponent;
